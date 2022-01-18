b372b3bc928bedc8f046732ed79f5d905c2be470
Annala <n1anje00@students.oamk.fi>
2022-01-18 12:24:40 +0200

    BMI react app task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [Weight, setWeight] = useState(0);
+  const [Height, setHeight] = useState(0);
+  const [Vastaus, setVastaus] = useState(0);
+
+  function calculate(e) {
+    e.preventDefault();
+    const tulos = Weight/(Height*Height);
+    setVastaus(tulos);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={calculate}>
+      <h2>Calculating body mass index</h2>
+      <div>
+        <label>Height</label>
+        <input type='number' value={Height} onChange={e => setHeight(e.target.value)}></input>
+      </div>
+      <div>
+        <label>Weight</label>
+        <input type='number' value={Weight} onChange={e => setWeight(e.target.value)}></input>
+      </div>
+      <div>
+        <label>Vastaus</label>
+        <output>{Vastaus.toFixed(2)}</output>
+      </div>
+      <button>Laske</button>
+      </form>
     </div>
   );
 }
